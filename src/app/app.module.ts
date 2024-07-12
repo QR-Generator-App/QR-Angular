641605edd88099cb1cad83d66a08d4e2992d9069
Sreekanth V <sreekanthv1995@gmail.com>
2024-07-12 13:29:09 +0530

    qr generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QrcodeComponent } from './qrcode/qrcode.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QrcodeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/qrcode/qrcode.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, of } from 'rxjs';
+
+@Component({
+  selector: 'app-qrcode',
+  templateUrl: './qrcode.component.html',
+  styleUrl: './qrcode.component.css',
+})
+export class QrcodeComponent implements OnInit {
+  qrForm!: FormGroup;
+  qrCodeImage: string | null = null;
+  constructor(private fb: FormBuilder,private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.qrForm = this.fb.group({
+      message: ['',Validators.required],
+    });
+  }
+
+  onAdd() {
+    const message = this.qrForm.get('message')?.value;
+    this.http.post('http://localhost:8080/generate', message, { responseType: 'text' })
+      .pipe(
+        map(response => 'data:image/png;base64,' + response),
+        catchError(error => {
+          console.error('Error:', error);
+          return of(null); 
+        })
+      )
+      .subscribe(qrCodeImage => this.qrCodeImage = qrCodeImage);
+  }
+
+  downloadImage() {
+    if (this.qrCodeImage) {
+      const link = document.createElement('a');
+      link.href = this.qrCodeImage;
+      link.download = 'qrcode.png';
+      link.click();
+    }
+  }
+}
